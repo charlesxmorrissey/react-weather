3c6fdce2ba315c25d5123963ce0871837bdcc429
Charles X. Morrissey <me@charles-x.com>
2020-02-21 17:05:52 -0500

    wip: progress on sagas

--- src/sagas/weather/sagas.js
+++ src/sagas/weather/sagas.js
@@ -1,24 +1,68 @@
 import { call, put } from 'redux-saga/effects'
-import DarkSkyAPI from 'dark-sky-skeleton'
+import DarkSky from 'dark-sky-skeleton'
 
 import { fetchWeatherSuccess, fetchWeatherError } from '@/store/weather/actions'
 
-const api = new DarkSkyAPI(process.env.DARK_SKY_SECRET_KEY)
+const darkSky = new DarkSky(process.env.DARK_SKY_SECRET_KEY)
+
+/*
+darkSkyApi
+  .latitude(lat)
+  .longitude(long)
+  .units('us')
+  .language('en')
+  .time('2000-04-06T12:20:05') // moment().year(2000).format('YYYY-MM-DDTHH:mm:ss')
+  .extendHourly(true)
+  .get()
+  .then(data => console.log(data));
+*/
 
 // 40.678177
 // -73.944160
-
 export function* fetchWeather() {
   try {
-    // const lat = yield call(api.latitude, '40.678177')
-    // const long = yield call(api.longitude, '-73.944160')
-    yield put(fetchWeatherSuccess(api.latitude('40.678177')))
-    yield put(fetchWeatherSuccess(api.longitude('-73.944160')))
-    yield put(fetchWeatherSuccess(api.get()))
+    // const lat = yield call(darkSky.latitude('40.678177'))
+    // const long = yield call(darkSky.longitude('-73.944160'))
+
+    const response = yield call(
+      darkSky
+        .latitude('40.678177')
+        .longitude('-73.944160')
+        // .units('us')
+        // .language('en')
+        // .time('2000-04-06T12:20:05')
+        // .extendHourly(true)
+        .get()
+        .then((data) => {
+          console.log('data::', data)
+          return data
+        })
+    )
+    console.log('response::', response)
+
+    // yield put(fetchWeatherSuccess(response))
   } catch (error) {
     yield put(fetchWeatherError(error))
   }
 }
+
+// export function* fetchWeather() {
+//   try {
+//     const response = yield call(
+//       darkSky
+//         .latitude('40.678177')
+//         .longitude('-73.944160')
+//         .get()
+//       // .then((data) => data)
+//     )
+
+//     console.log('response::', response)
+
+//     yield put(fetchWeatherSuccess(response))
+//   } catch (error) {
+//     yield put(fetchWeatherError(error))
+//   }
+// }
 
 export function* weatherWatcher() {
   yield fetchWeather()
